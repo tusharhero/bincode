a94c1154d349bb37ce3894a198f561c23e9aa392
tusharhero <tusharmaharana44676@gmail.com>
2022-05-19 13:33:54 +0530

    created a function to convert bin to int(requires the input to be reversed)

--- bincode-gen.py
+++ bincode-gen.py
@@ -10,4 +10,23 @@ img0 = Image.new("1", (1000,100),1)
 #This image is a small black box ⬛ and it is the block we are going to use denote 1
 img1 = Image.new("1", (100,100))
 
-img0.show()
+##we need a function to convert a given number into binary
+#I dont think this is a good idea, but i am going to try it anyway
+
+##fist we need bin2int
+def bin2int(binnum):#the bin must be inverted for this to work
+    number = 0
+    n = 0
+    #all of the values of binary places, this is from left to right instead of right to left
+    binnumvalues = [1,2,4,8,16,32,64,128,256,512,1024]
+
+    #loops until the number fully complete
+    while n < len(binnum):
+        number += binnum[n]*binnumvalues[n]#number = binnum(1) * 2^place ...
+        n += 1
+    return number
+'''
+def int2bin(number):
+    number = int(number)
+    binnum = [0,0,0,0,0,0,0,0,0,0]
+'''
