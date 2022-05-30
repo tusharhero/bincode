2a156d3d78d6b5aa7dc725013138b9a97943e916
blankRiot96 <blankRiot96@gmail.com>
2022-05-30 17:12:41 +0530

    Cleaned up code.

--- 0to1023/0to1023.py
+++ 0to1023/0to1023.py
@@ -1,11 +1,8 @@
 import bincode as bc
 
-n = 0
-
-while n <= 1023:
+for n in range(1024):
     name = "./images/" + str(n) + ".png"
     bc.mkbincodeimg(n).save(name)
-    n += 1
 
-##command to convert all the images to animated gif(only linux🐧)
-#convert -delay 120 -loop 0 ./images/*.png 0to1023.gif
+# Command to convert all the images to animated gif(only linux🐧)
+# convert -delay 120 -loop 0 ./images/*.png 0to1023.gif
